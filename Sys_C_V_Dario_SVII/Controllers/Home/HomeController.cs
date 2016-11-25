82d05ac62646068f7c6ea195ef99da79b690660d
nxrx200796 <ota.ani96@hotmail.com>
2016-11-25 05:21:50 -0500

    Añadiendo la lista de funciones a la master page

---
+++ Sys_C_V_Dario_SVII/Controllers/Home/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sys_C_V_Dario_SVII.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
