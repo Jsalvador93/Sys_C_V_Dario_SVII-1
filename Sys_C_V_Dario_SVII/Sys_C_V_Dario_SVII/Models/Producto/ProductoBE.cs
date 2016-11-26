eaee864b54de6806c57ba14faf5ed333de4ac705
iDavidhg <davidhg_1702@hotmail.com>
2016-11-26 18:52:09 -0500

    Revert "prueba2 producto"
    This reverts commit 7ade9fa3c1a1db42df37b094d2a5e9ccdb9ac306.

--- Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Models/Producto/ProductoBE.cs
+++ Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Models/Producto/ProductoBE.cs
@@ -8,6 +8,5 @@ namespace Sys_C_V_Dario_SVII.Models.Producto
     public class ProductoBE
     {
         public int id_producto { get; set; }
-        public string descripcion { get; set; }
     }
 }
