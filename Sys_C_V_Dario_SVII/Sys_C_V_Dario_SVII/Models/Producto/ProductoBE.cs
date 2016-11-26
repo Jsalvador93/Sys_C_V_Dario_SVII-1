7ade9fa3c1a1db42df37b094d2a5e9ccdb9ac306
iDavidhg <davidhg_1702@hotmail.com>
2016-11-26 18:50:48 -0500

    prueba2 producto

--- Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Models/Producto/ProductoBE.cs
+++ Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Models/Producto/ProductoBE.cs
@@ -8,5 +8,6 @@ namespace Sys_C_V_Dario_SVII.Models.Producto
     public class ProductoBE
     {
         public int id_producto { get; set; }
+        public string descripcion { get; set; }
     }
 }
