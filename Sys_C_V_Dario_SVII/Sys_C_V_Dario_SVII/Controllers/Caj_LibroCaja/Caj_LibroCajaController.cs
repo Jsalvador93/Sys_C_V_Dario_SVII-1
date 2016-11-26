cd93549d3f8942eb8608e73bb1091c939d368ecd
iDavidhg <davidhg_1702@hotmail.com>
2016-11-26 18:37:00 -0500

    Prueba producto

---
+++ Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Controllers/Caj_LibroCaja/Caj_LibroCajaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sys_C_V_Dario_SVII.Controllers.Caj_LibroCaja
+{
+    public class Caj_LibroCajaController : Controller
+    {
+        // GET: Caj_LibroCaja
+        public ActionResult LibroCaja()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Controllers/Caj_Movimiento/Caj_MovimientoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sys_C_V_Dario_SVII.Controllers.Caj_Movimiento
+{
+    public class Caj_MovimientoController : Controller
+    {
+        // GET: Caj_Movimiento
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RegistrarMovimiento()
+        {
+            return View();
+        }
+        public ActionResult BuscarMovimiento()
+        {
+            return View();
+        }
+
+    }
+}
