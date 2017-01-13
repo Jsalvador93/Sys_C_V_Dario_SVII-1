33ce1c84f77465032f756aca7f968e1cc71b3e8c
franz <franz@FRANZ-PC>
2017-01-13 08:36:52 +0100

    Add Models para Modulo Reporte

---
+++ Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Models/Est_Tipo_Estado/Est_Tipo_EstadoBE.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sys_C_V_Dario_SVII.Models.Est_Tipo_Estado
+{
+    public class Est_Tipo_EstadoBE
+    {
+    }
+}
