844bd66a5880171e83dd8467ef9c1cd8e12e894c
iDavidhg <davidhg_1702@hotmail.com>
2017-01-27 12:46:53 -0500

    add com_vta

---
+++ Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Controllers/Com_Pedido/Com_PedidoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sys_C_V_Dario_SVII.Models.Mod_Compra.Com_Pedido;
+using Sys_C_V_Dario_SVII.Models.Mod_Compra.Pro_Producto;
+
+namespace Sys_C_V_Dario_SVII.Controllers.Com_Pedido
+{
+    public class Com_PedidoController : Controller
+    {
+        // GET: Com_Pedido
+        public ActionResult Index()
+        {
+            Com_PedidoDA objVentaDA = new Com_PedidoDA();
+            return View(objVentaDA.ListaRegistroPedido(0, ""));
+        }
+
+        [HttpPost]
+        public ActionResult _modalDetalle(int i_idPedido)
+        {
+            string pDato = Convert.ToString(i_idPedido);
+            Com_PedidoDA objPedidoDA = new Com_PedidoDA();
+            Com_PedidoBE objPedidoBE = new Com_PedidoBE();
+            foreach (Com_PedidoBE objPedido in objPedidoDA.ListaRegistroPedido(0, pDato))
+            {
+                objPedidoBE = objPedido;
+            }
+            return PartialView("_modalDetalle", objPedidoBE);
+        }
+
+        [HttpPost]
+        public ActionResult _modalListaProductos(string a)
+        {
+            Pro_ProductoDA objProductoDA = new Pro_ProductoDA();
+            return PartialView("_modalListaProductos", objProductoDA.ListaRegistroProducto(0, ""));
+        }
+
+        public ActionResult RegistrarPedidos()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Controllers/Pro_Producto/Pro_ProductoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sys_C_V_Dario_SVII.Models.Mod_Compra.Pro_Producto;
+using Sys_C_V_Dario_SVII.Models.Mod_Compra.Pro_Marca;
+using Sys_C_V_Dario_SVII.Models.Mod_Compra.Pro_Seccion;
+using Sys_C_V_Dario_SVII.Models.Mod_Compra.Pro_Tipo_Producto;
+
+namespace Sys_C_V_Dario_SVII.Controllers.Pro_Producto
+{
+    public class Pro_ProductoController : Controller
+    {
+        // GET: Pro_Producto
+        public ActionResult Index()
+        {
+            Pro_ProductoDA objProductoDA = new Pro_ProductoDA();
+            return View(objProductoDA.ListaRegistroProducto(0, ""));
+        }
+
+        [HttpPost]
+        public ActionResult _modalPrecio( string c_codProducto)
+        {
+            //string dato = "1";
+            Pro_ProductoDA objProductoDA = new Pro_ProductoDA();
+            Pro_ProductoBE objProductoBE = new Pro_ProductoBE();
+            foreach (Pro_ProductoBE objProducto in objProductoDA.ListaRegistroProducto(0, c_codProducto))
+            {
+                objProductoBE = objProducto;
+            }
+            return PartialView("_modalPrecio", objProductoBE);
+
+        }
+
+        [HttpPost]
+        public ActionResult _modalMarca(string a)
+        {
+            Pro_MarcaDA objMarcaDA = new Pro_MarcaDA();
+            return PartialView("_modalMarca", objMarcaDA.ListaRegistroMarca(""));
+        }
+
+        [HttpPost]
+        public ActionResult _modalSeccion(string a)
+        {
+            Pro_SeccionDA objSeccionDA = new Pro_SeccionDA();
+            return PartialView("_modalSeccion", objSeccionDA.ListaRegistroSeccion(""));
+        }
+
+        [HttpPost]
+        public ActionResult _modalTipoProducto(string a)
+        {
+            Pro_Tipo_ProductoDA objTipo_ProductoDA = new Pro_Tipo_ProductoDA();
+            return PartialView("_modalTipoProducto", objTipo_ProductoDA.ListaRegistroTipoProducto(""));
+        }
+
+        public ActionResult RegistrarProducto()
+        {
+            
+            return View();
+        }
+    }
+}
