693156224db11bebc17d24e8e03a3576f23b9ecd
iDavidhg <davidhg_1702@hotmail.com>
2017-01-31 14:18:15 -0500

    Arreglo list vta

--- Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Models/Emp_Empresa/Emp_EmpresaBE.cs
+++ Sys_C_V_Dario_SVII/Sys_C_V_Dario_SVII/Models/Emp_Empresa/Emp_EmpresaBE.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Sys_C_V_Dario_SVII.Models.Mod_Sistema.Emp_Empresa
+namespace Sys_C_V_Dario_SVII.Models.Emp_Empresa
 {
     public class Emp_EmpresaBE
     {
